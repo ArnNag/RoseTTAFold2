c11ee52ae8d89c8a3aadc68bc73d3ccc113a49c8
Arnav Nagle <rnow03@gmail.com>
2024-10-30 10:45:29 -0700

    Copy pAE splitting function to density.py with refactoring

--- network/density.py
+++ network/density.py
@@ -1,5 +1,6 @@
 import os
 import torch
+import numpy as np
 from icecream import ic
 
 from network import util
@@ -39,7 +40,8 @@ def plddt_trim(model):
     # trim low plddts
     plddt_mask = model['plddt'] > params['PLDDT_CUT']
     if torch.all(torch.logical_not(plddt_mask)):
-        raise ValueError(f"All predicted pLDDT values were below the cutoff threshold. Lowest pLDDT: {torch.min(torch.nan_to_num(model['plddt'], nan=1e14))}. pLDDT cutoff: {params['PLDDT_CUT']}")
+        raise ValueError(
+            f"All predicted pLDDT values were below the cutoff threshold. Lowest pLDDT: {torch.min(torch.nan_to_num(model['plddt'], nan=1e14))}. pLDDT cutoff: {params['PLDDT_CUT']}")
     # remove singletons
     mask, idx, ct = torch.torch.unique_consecutive(plddt_mask, dim=0, return_counts=True, return_inverse=True)
     mask = mask * ct >= params['MIN_RES_CUT']
@@ -64,59 +66,11 @@ def plddt_trim(model):
         'plddt_mask': plddt_mask,
     }
 
-def domains_from_pae_matrix_igraph(pae_matrix, pae_power=1, pae_cutoff=5, graph_resolution=1):
-    '''
-    Takes a predicted aligned error (PAE) matrix representing the predicted error in distances between each 
-    pair of residues in a model, and uses a graph-based community clustering algorithm to partition the model
-    into approximately rigid groups.
 
-    Arguments:
-
-        * pae_matrix: a (n_residues x n_residues) numpy array. Diagonal elements should be set to some non-zero
-          value to avoid divide-by-zero warnings
-        * pae_power (optional, default=1): each edge in the graph will be weighted proportional to (1/pae**pae_power)
-        * pae_cutoff (optional, default=5): graph edges will only be created for residue pairs with pae<pae_cutoff
-        * graph_resolution (optional, default=1): regulates how aggressively the clustering algorithm is. Smaller values
-          lead to larger clusters. Value should be larger than zero, and values larger than 5 are unlikely to be useful.
-
-    Returns: a series of lists, where each list contains the indices of residues belonging to one cluster.
-    '''
-    try:
-        import igraph
-    except ImportError:
-        print('ERROR: This method requires python-igraph to be installed. Please install it using "pip install python-igraph" '
-            'in a Python >=3.6 environment and try again.')
-        import sys
-        sys.exit()
-    import numpy
-    weights = 1/pae_matrix**pae_power
-
-    g = igraph.Graph()
-    size = weights.shape[0]
-    g.add_vertices(range(size))
-    edges = numpy.argwhere(pae_matrix < pae_cutoff)
-    ic(edges)
-    if len(edges) == 0:
-        raise ValueError("All pAE values were below the pae_cutoff threshold.")
-    sel_weights = weights[edges.T[0], edges.T[1]]
-    g.add_edges(list(zip(edges.T[0], edges.T[1])))
-    g.es['weight'] = list(sel_weights)
-
-    vc = g.community_leiden(weights='weight', resolution_parameter=graph_resolution/100, n_iterations=-1)
-    membership = numpy.array(vc.membership)
-    from collections import defaultdict
-    clusters = defaultdict(list)
-    for i, c in enumerate(membership):
-        clusters[c].append(i)
-    clusters = list(sorted(clusters.values(), key=lambda l:(len(l)), reverse=True))
-    return clusters
-
-
-
-def multidock_model(allfiles: list[str], mapfile) -> rosetta.core.pose.Pose:
+def multidock_model(allfiles: list[str], mapfile: str) -> rosetta.core.pose.Pose:
     rosetta.core.scoring.electron_density.getDensityMap(mapfile)
     dock_into_dens: rosetta.protocols.electron_density.DockFragmentsIntoDensityMover = setup_docking_mover(counts=1)
-    
+
     all_poses: Pose = Pose()
     for filename in allfiles:
         pose_before_fit: Pose = pose_from_pdb(filename)
@@ -128,19 +82,106 @@ def multidock_model(allfiles: list[str], mapfile) -> rosetta.core.pose.Pose:
     return all_poses
 
 
-def rosetta_density_dock(before_dock_file, after_dock_file, model, counts, mapfile):
-    clusters = domains_from_pae_matrix_igraph(model["pae"].cpu(), pae_cutoff=100, graph_resolution=0.005)
-    ic(clusters)
-    # TODO: split model into clusters and pass into multidock model
-    ic(model["pae"])
-    trimmed_model = plddt_trim(model)
-    ic(trimmed_model["plddt_mask"])
-    util.writepdb(before_dock_file, trimmed_model['xyz'], trimmed_model['seq'], trimmed_model['Ls'], bfacts=100 * trimmed_model['plddt'])
-    pose: rosetta.core.pose.Pose = multidock_model(before_dock_file, mapfile, counts)
+def split_by_pae(
+        pae_array: np.ndarray,
+        min_split_length: int,
+) -> list[int]:
+
+    chain_length = pae_array.shape[0]
+    assert pae_array.shape[1] == chain_length
+    assert chain_length >= min_split_length
+    assert min_split_length > 0
+
+    def inter_vs_intra_pae_score(test_slice: slice) -> float:
+
+        assert test_slice.start >= 0
+        assert test_slice.stop <= pae_array.shape[0]
+
+        avg_inter_split_pae = np.mean(pae_array[test_slice, test_slice])
+        first_cross_term = pae_array[test_slice, 0:test_slice.start]
+        second_cross_term = pae_array[test_slice, test_slice.stop:]
+        third_cross_term = pae_array[0:test_slice.start, test_slice]
+        fourth_cross_term = pae_array[test_slice.stop:, test_slice]
+
+        avg_intra_split_pae = (
+                                      first_cross_term.sum()
+                                      + second_cross_term.sum()
+                                      + third_cross_term.sum()
+                                      + fourth_cross_term.sum()
+                              ) / (
+                                      first_cross_term.size
+                                      + second_cross_term.size
+                                      + third_cross_term.size
+                                      + fourth_cross_term.size
+                              )
+
+        pae_region_scale_factor = avg_intra_split_pae / (
+                avg_inter_split_pae + 1e-9
+        )
+
+        return pae_region_scale_factor
+
+    def split_by_pae_for_region(
+            search_start_idx: int,
+            search_end_idx: int,
+    ) -> list[int]:
+
+        assert chain_length >= search_end_idx
+        assert search_end_idx >= search_start_idx
+        assert search_start_idx >= 0
+
+        if search_start_idx + min_split_length >= search_end_idx:
+            return []
+
+        best_pae_region_scale_factor = -float("inf")
+        best_slice = slice(search_start_idx, search_end_idx)
+        best_slice_length = -1
+        for start_idx in range(search_start_idx, search_end_idx - min_split_length + 1):
+            for end_idx in range(start_idx + min_split_length, search_end_idx + 1):
+                test_slice = slice(start_idx, end_idx)
+                test_slice_length = end_idx - start_idx
+                pae_region_scale_factor: float = inter_vs_intra_pae_score(test_slice)
+                if pae_region_scale_factor > best_pae_region_scale_factor or (
+                        pae_region_scale_factor == best_pae_region_scale_factor
+                        and test_slice_length > best_slice_length
+                ):
+                    best_slice = test_slice
+                    best_pae_region_scale_factor = pae_region_scale_factor
+                    best_slice_length = test_slice_length
+
+        best_slice_before = split_by_pae_for_region(
+            search_start_idx=search_start_idx,
+            search_end_idx=best_slice.start,
+        )
+
+        best_slice_after = split_by_pae_for_region(
+            search_start_idx=best_slice.stop,
+            search_end_idx=search_end_idx,
+        )
+
+        best_slice_before.append(best_slice.stop)
+        best_slice_before.extend(best_slice_after)
+        return best_slice_before
+
+    return split_by_pae_for_region(
+        search_start_idx=0, search_end_idx=chain_length
+    )
+
+
+def rosetta_density_dock(before_dock_file, after_dock_file, model, mapfile):
+    split_points: list[int] = split_by_pae(model["pae"].cpu(), min_split_length=100)
+    ic(split_points)
+    # trimmed_model = plddt_trim(model)
+    trimmed_model = model
+    # ic(trimmed_model["plddt_mask"])
+    util.writepdb(before_dock_file, trimmed_model['xyz'], trimmed_model['seq'], trimmed_model['Ls'],
+                  bfacts=100 * trimmed_model['plddt'])
+    pose: rosetta.core.pose.Pose = multidock_model(before_dock_file, mapfile)
     pose.pdb_info(rosetta.core.pose.PDBInfo(pose))
     pose.dump_pdb(after_dock_file)
     xyz_with_dummy = torch.full_like(model['xyz'], torch.nan).unsqueeze(0)
     # TODO: better way to deal with batch axis than unsqueeze?
-    xyz_with_dummy[0][trimmed_model['plddt_mask']] = torch.from_numpy(parse_pdb_w_seq(after_dock_file)[0]).to(xyz_with_dummy)
+    xyz_with_dummy[0][trimmed_model['plddt_mask']] = torch.from_numpy(parse_pdb_w_seq(after_dock_file)[0]).to(
+        xyz_with_dummy)
 
     return xyz_with_dummy, trimmed_model['plddt_mask']
