bbb986d2bf93e7e59fcc32830ebc438a4e03da66
Arnav Nagle <rnow03@gmail.com>
2024-11-18 14:33:26 -0800

    Try incorporating masked inputs in hardcoded positions test. Not complete.

--- network/run_hardcode_xyz.py
+++ network/run_hardcode_xyz.py
@@ -13,6 +13,7 @@ from kinematics import xyz_to_t2d
 use_template = True
 pdb_name = "globin"
 a3m_name = "globin" # only used to get sequence
+
 model = os.path.dirname(__file__) + "/weights/RF2_jan24.pt"
 pred = Predictor(model, torch.device("cuda:0"))
 
@@ -29,54 +30,42 @@ nseqs = 256
 actual_seq = msa[0].unsqueeze(0)
 
 pdb_path = f"pdb/{pdb_name}.pdb"
-new_xyz = torch.from_numpy(
-    parse_pdb_w_seq(pdb_path)[
-        0
-    ]
-).to(pred.device).unsqueeze(0)
 L = Ls[0]
 
-if use_template:
-    xyz_t, t1d, mask_t = read_template_pdb(L, pdb_path, align_conf=1.0)
-    xyz_t = xyz_t.unsqueeze(0).to(pred.device)
-    mask_t = mask_t.unsqueeze(0).to(pred.device)
-    t1d = t1d.unsqueeze(0).to(pred.device)
-    mask_t_2d = mask_t[:, :, :, :3].all(dim=-1)  # (B, T, L)
-    mask_t_2d = mask_t_2d[:, :, None] * mask_t_2d[:, :, :, None]  # (B, T, L, L)
-    t2d = xyz_to_t2d(xyz_t, mask_t_2d).half()
-    seq_tmp = t1d[..., :-1].argmax(dim=-1).reshape(-1, L)
-    alpha, _, alpha_mask, _ = pred.xyz_converter.get_torsions(
-        xyz_t.reshape(-1, L, 27, 3), seq_tmp, mask_in=mask_t.reshape(-1, L, 27)
-    )
-    xyz_t = xyz_t[:, :, :, 1]
-    alpha_mask = torch.logical_and(alpha_mask, ~torch.isnan(alpha[..., 0]))
+splits_with_ends = [0, 100, L]
+new_mask_by_split = torch.array([False, True, False])
 
-    alpha[torch.isnan(alpha)] = 0.0
-    alpha = alpha.reshape(1, -1, L, 10, 2)
-    alpha_mask = alpha_mask.reshape(1, -1, L, 10, 1)
-    alpha_t = torch.cat((alpha, alpha_mask), dim=-1).reshape(1, -1, L, 3 * 10)
-else:
-    # dummy template
-    n_templ = 1
-    SYMM_OFFSET_SCALE = 1.0
-    xyz_t = (
-            INIT_CRDS.reshape(1, 1, 27, 3).repeat(n_templ, L, 1, 1)
-            + torch.rand(n_templ, L, 1, 3) * 5.0
-            - 2.5
-            + L ** (1 / 2)  # note: offset based on symmgroup
-    )
-    xyz_t = xyz_t.float().unsqueeze(0)
-    xyz_t = xyz_t[:, :, :, 1].to(pred.device)
-    t1d = torch.zeros(1, 1, L, 22).to(pred.device)
-    t2d = torch.zeros(1, 1, L, L, 44).to(pred.device)
-    mask_t_2d = torch.zeros(1, 1, L, L).to(pred.device)
+xyz_t, t1d, _ = read_template_pdb(L, pdb_path, align_conf=1.0)
+new_mask = torch.full((xyz_t.shape[1], 27, 3), True)
+for split_idx in range(len(splits_with_ends) - 1):
+    start_idx = splits_with_ends[split_idx]
+    end_idx = splits_with_ends[split_idx + 1]
+    new_mask[start_idx:end_idx, :] = new_mask_by_split[split_idx]
 
-L = Ls[0]
+xyz_t = util.realign_missing(xyz_t[0, :, :, :], new_mask, sigma=1e-2)[None, None, :, :, :].to(pred.device)
+mask_t = new_mask[None, None, :, :].to(pred.device) # (B, T, L, A)
+t1d = t1d.unsqueeze(0).to(pred.device)
+mask_t_2d = mask_t[:, :, :, :3].all(dim=-1)  # (B, T, L)
+mask_t_2d = mask_t_2d[:, :, None] * mask_t_2d[:, :, :, None]  # (B, T, L, L)
+t2d = xyz_to_t2d(xyz_t, mask_t_2d).half()
+seq_tmp = t1d[..., :-1].argmax(dim=-1).reshape(-1, L)
+alpha, _, alpha_mask, _ = pred.xyz_converter.get_torsions(
+    xyz_t.reshape(-1, L, 27, 3), seq_tmp, mask_in=mask_t.reshape(-1, L, 27)
+)
+xyz_t = xyz_t[:, :, :, 1]
+alpha_mask = torch.logical_and(alpha_mask, ~torch.isnan(alpha[..., 0]))
+
+alpha[torch.isnan(alpha)] = 0.0
+alpha = alpha.reshape(1, -1, L, 10, 2)
+alpha_mask = alpha_mask.reshape(1, -1, L, 10, 1)
+alpha_t = torch.cat((alpha, alpha_mask), dim=-1).reshape(1, -1, L, 3 * 10)
+
 msa_seed = torch.zeros(1, nseqs, L, 48).to(pred.device)
 msa_full = torch.zeros(1, nseqs, L, 48).to(pred.device)
 msa_extra = torch.zeros(1, nseqs, L, 25).to(pred.device)
 seq_input = torch.zeros_like(actual_seq).to(pred.device)
 idx = torch.arange(L)[None, :].to(pred.device)
+
 with torch.cuda.amp.autocast(True):
     (
         logit_s,
