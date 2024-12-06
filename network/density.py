Fix off by one error

network/density.py
@@ -144,7 +144,7 @@ def split_by_pae(
         best_slice = slice(search_start_idx, search_end_idx)
         best_slice_length = -1
         for start_idx in range(search_start_idx, search_end_idx - min_split_length + 1):
-            for end_idx in range(start_idx + min_split_length, search_end_idx):
+            for end_idx in range(start_idx + min_split_length, search_end_idx + 1):
                 print(f"{start_idx=}")
                 print(f"{end_idx=}")
                 test_slice = slice(start_idx, end_idx)
@@ -172,7 +172,7 @@ def split_by_pae(
         best_slice_before.extend(best_slice_after)
         return best_slice_before
 
-    pae_enrichment = torch.full((chain_length, chain_length), torch.nan)
+    pae_enrichment = torch.full((chain_length + 1, chain_length + 1), torch.nan)
     compute_all_regions()
 
     return split_by_pae_for_region(
@@ -182,7 +182,7 @@ def split_by_pae(
 
 def test_split_by_pae():
 
-    block_sizes = [7, 3, 4, 5]
+    block_sizes = [7, 2, 4, 5]
     total_size = sum(block_sizes)
     block_matrix = torch.ones((total_size, total_size))
     current_index = 0