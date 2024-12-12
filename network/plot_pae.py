156562b244ab54c9d36f5d00c42beeb79bed9f00
Arnav Nagle <rnow03@gmail.com>
2024-12-12 10:46:19 -0800

    Weird failure cases for vectorized pAE implementation: one is when all pAE within the region is 0

--- network/plot_pae.py
+++ network/plot_pae.py
@@ -6,8 +6,6 @@ from matplotlib import pyplot as plt
 
 from density import split_by_pae, compute_pae_enrichment
 
-# pae_array_rf2 = torch.tensor(np.load("test_atpbind_emd_14914_after_dock_cycle_0_split_0.npz")["pae"]).to(torch.float32)
-
 af2_pae_path = "/home/nagleam/Downloads/AF-T0D3N5-F1-predicted_aligned_error_v4.json"
 pae_array_af2 = torch.tensor(json.load(open(af2_pae_path))[0]["predicted_aligned_error"], dtype=torch.float)
 
@@ -19,7 +17,6 @@ def two_d_cumsum(pae_array: torch.Tensor) -> torch.Tensor:
     pae_cumsum = functional.pad(input=pae_cumsum, pad=(1, 0, 1, 0), mode='constant', value=0.)
     return pae_cumsum
 
-# pae_cumsum_rf2 = two_d_cumsum(pae_array_rf2)
 pae_cumsum_af2 = two_d_cumsum(pae_array_af2)
 
 
@@ -48,30 +45,25 @@ def inter_vs_intra_pae_score_naive(pae_array: torch.Tensor, test_slice: slice) -
                           )
 
     pae_region_scale_factor = avg_intra_split_pae / (
-            avg_inter_split_pae + 1e-9
+            avg_inter_split_pae
     )
 
     return pae_region_scale_factor
 
-# splits_rf2: list[int] = split_by_pae(pae_array_rf2, min_split_length=100)
-# splits_af2: list[int] = split_by_pae(pae_array_af2, min_split_length=1)
 pae_enrichment = compute_pae_enrichment(pae_array_af2)
-
-# print(f"{splits_af2=}")
-# print(f"{splits_rf2=}")
 
 plt.contour(torch.nan_to_num(pae_enrichment))
 plt.savefig(fname="pae_enrichment_af2.png")
 
-# print(f"{len(pae_array_rf2)}=")
-print(f"{len(pae_array_af2)}=")
-# print(f"{inter_vs_intra_pae_score(pae_cumsum_rf2, slice(0, 400))}")
-print(f"{inter_vs_intra_pae_score_naive(pae_cumsum_af2, slice(312, 450))}")
-print(f"{pae_enrichment[312,450]}")
+print(f"{len(pae_array_af2)=}")
+print(f"{inter_vs_intra_pae_score_naive(pae_array_af2, slice(911, 912))=}")
+print(f"{0.5 * pae_enrichment[911, 912]=}")
+print(f"{inter_vs_intra_pae_score_naive(pae_array_af2, slice(910, 913))=}")
+print(f"{0.5 * pae_enrichment[910, 913]=}")
 
 for j in range(len(pae_enrichment)):
     for i in range(j):
         test_slice = slice(i, j)
-        assert torch.isclose(pae_enrichment[i, j], torch.scalar_tensor(inter_vs_intra_pae_score_naive(pae_array_af2, test_slice))), f"Failed for {i} {j}"
-
-# print(pae_array_rf2.dtype)
+        fast_pae_enrichment = 0.5 * pae_enrichment[i, j]
+        slow_pae_enrichment = torch.scalar_tensor(inter_vs_intra_pae_score_naive(pae_array_af2, test_slice))
+        assert torch.isclose(fast_pae_enrichment, slow_pae_enrichment, atol=1e-1) or (fast_pae_enrichment > 1e7 and slow_pae_enrichment > 1e7) or ((j - i) == 1), f"Failed for {i} {j}. Fast: {fast_pae_enrichment}, slow: {slow_pae_enrichment}"
